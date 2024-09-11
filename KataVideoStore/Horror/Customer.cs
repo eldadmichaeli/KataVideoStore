c94ad7d630d5640b05bf82299c76a264bfd1c6d8
ViktoriiaM <Viktoriia.Mykhalchenko@nice.com>
2024-09-11 16:17:22 +0300

    Extract amount calculation

--- KataVideoStore/Horror/Customer.cs
+++ KataVideoStore/Horror/Customer.cs
@@ -24,32 +24,47 @@ namespace KataVideoStore.Horror
         {
             double totalAmount = 0;
             int frequentRenterPoints = 0;
-            StringBuilder result = new StringBuilder();
-            result.Append($"Rental Record for { Name }\n");
+            var result = new StringBuilder();
+            
+            result.Append($"Rental Record for {Name}\n");
+
             foreach (var rental in _rentals)
             {
-                double thisAmount = 0;
-                switch (rental.Key.PriceCode)
-                {
-                    case Movie.Regular:
-                        thisAmount += rental.Value > 2 ? 2 + (rental.Value - 2) * 1.5 : 2;
-                        break;
-                    case Movie.NewRelease:
-                        thisAmount += rental.Value * 3;
-                        break;
-                    case Movie.Children:
-                        thisAmount += rental.Value > 3 ? 1.5 + (rental.Value - 3) * 1.5 : 1.5;
-                        break;
-                }
+                var thisAmount = CalculateAmount(rental.Key.PriceCode, rental.Value);
+
                 frequentRenterPoints++;
+                
                 if (rental.Key.PriceCode == Movie.NewRelease && rental.Value > 1)
                     frequentRenterPoints++;
+                
                 result.Append($"\t{rental.Key.Title}\t{thisAmount}\n");
                 totalAmount += thisAmount;
             }
+
             result.Append($"Amount owed is {totalAmount}\n");
             result.Append($"You earned {frequentRenterPoints} frequent renter points");
+
             return result.ToString();
+        }
+
+        private static double CalculateAmount(int priceCode, int rentalValue)
+        {
+            double amount = 0;
+            
+            switch (priceCode)
+            {
+                case Movie.Regular:
+                    amount += rentalValue > 2 ? 2 + (rentalValue - 2) * 1.5 : 2;
+                    break;
+                case Movie.NewRelease:
+                    amount += rentalValue * 3;
+                    break;
+                case Movie.Children:
+                    amount += rentalValue > 3 ? 1.5 + (rentalValue - 3) * 1.5 : 1.5;
+                    break;
+            }
+
+            return amount;
         }
     }
 }
