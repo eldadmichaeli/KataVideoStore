77582725dc73db4284878c321c825de35cd336b9
Ievgenii Kyienko-Romaniuk <Ievgenii.Kyienko-Romaniuk@nice.com>
2024-09-11 16:19:32 +0300

    1st refactoring

--- KataVideoStore/Horror/Customer.cs
+++ KataVideoStore/Horror/Customer.cs
@@ -4,57 +4,59 @@
 
     public class Customer
     {
-        private string name;
-        private Dictionary<Movie, int> rentals = new Dictionary<Movie, int>();
+        private readonly string _name;
+        private readonly List<Rental> _rentals = new List<Rental>();
 
         public Customer(string name)
         {
-            this.name = name;
+            _name = name;
         }
 
         public void AddRental(Movie movie, int daysRented)
         {
-            rentals.Add(movie, daysRented);
-        }
-
-        public string GetName()
-        {
-            return name;
+            _rentals.Add(new Rental(movie, daysRented));
         }
 
         public string Statement()
         {
             double totalAmount = 0;
             int frequentRenterPoints = 0;
-            string result = "Rental Record for " + GetName() + "\n";
-            foreach (var rental in rentals)
+            string result = "Rental Record for " + _name + "\n";
+            foreach (var rental in _rentals)
             {
                 double thisAmount = 0;
-                switch (rental.Key.GetPriceCode())
+                switch (rental.Movie.GetPriceCode())
                 {
                     case Movie.REGULAR:
-                        thisAmount += 2;
-                        if (rental.Value > 2)
-                            thisAmount += (rental.Value - 2) * 1.5;
+                        thisAmount = CalculateRegular(rental, thisAmount);
                         break;
                     case Movie.NEW_RELEASE:
-                        thisAmount += rental.Value * 3;
+                        thisAmount += rental.Days * 3;
                         break;
                     case Movie.CHILDRENS:
                         thisAmount += 1.5;
-                        if (rental.Value > 3)
-                            thisAmount += (rental.Value - 3) * 1.5;
+                        if (rental.Days > 3)
+                            thisAmount += (rental.Days - 3) * 1.5;
                         break;
                 }
                 frequentRenterPoints++;
-                if ((rental.Key.GetPriceCode() == Movie.NEW_RELEASE) && rental.Value > 1)
+                if ((rental.Movie.GetPriceCode() == Movie.NEW_RELEASE) && rental.Days > 1)
                     frequentRenterPoints++;
-                result += "\t" + rental.Key.GetTitle() + "\t" + thisAmount + "\n";
+                result += "\t" + rental.Movie.GetTitle() + "\t" + thisAmount + "\n";
                 totalAmount += thisAmount;
             }
             result += "Amount owed is " + totalAmount + "\n";
             result += "You earned " + frequentRenterPoints + " frequent renter points";
             return result;
+        }
+
+        private double CalculateRegular(Rental rental, double thisAmount)
+        {
+            thisAmount += 2;
+            if (rental.Days > 2)
+                thisAmount += (rental.Days - 2) * 1.5;
+
+            return thisAmount;
         }
     }
 }
---
+++ KataVideoStore/Horror/Rental.cs
@@ -0,0 +1,14 @@
+﻿namespace KataVideoStore.Horror
+{
+    public class Rental
+    {
+        public Movie Movie { get; set; }
+        public int Days { get; set; }
+
+        public Rental(Movie movie, int daysRented)
+        {
+            Movie = movie;
+            Days = daysRented;
+        }
+    }
+}
